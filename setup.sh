7774cc4e78f1bd79cd9170577e1c7265e8227bd3
Michael <michaellavaire@gmail.com>
2023-11-08 22:45:30 -0500

    Fixes

--- setup.sh
+++ setup.sh
@@ -6,11 +6,12 @@ apt-get install -q=1 -y \
     nano bash git jq \
     pcregrep socat zip unzip \
     curl wget net-tools htop \
-    btop dnsutils ca-certificates zsh
+    btop dnsutils ca-certificates \
+    zsh
 
 # set time and time zone and display the current time settings
-timedatectl set-timezone UTC
-timedatectl
+# timedatectl set-timezone UTC
+# timedatectl
 
 # cat bashrc.additions >> ~/.bashrc
 # cp ./.gitmessage ~
@@ -18,12 +19,22 @@ cp ./.gitconfig ~
 
 # powerline fonts for zsh agnoster theme
 git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
+pushd fonts
 ./install.sh
-cd .. && rm -rf fonts
+popd ..
+rm -rf fonts
+
+# nerd fonts for zsh agnoster theme
+git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
+pushd nerd-fonts
+./install.sh
+popd ..
+rm -rf nerd-fonts
 
 # oh-my-zsh & plugins
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
-zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
-cp ./zsh/* ~
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+# zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+# Install zsh config
+cp zsh/* ~
